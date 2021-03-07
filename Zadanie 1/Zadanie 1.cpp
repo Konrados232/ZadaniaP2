a45d23a55411e23d259051df7bad74cd6b0092bc
Konrad Nowak <shaderpl@gmail.com>
2021-03-07 18:08:56 +0100

    Zadanie z klasą string

---
+++ Zadanie 1/Zadanie 1.cpp
@@ -0,0 +1,33 @@
+﻿// Konrad Nowak
+// Zadanie 1
+
+#include <iostream>
+
+using namespace std;
+
+//PIERWSZE ROZWIĄZANIE - klasa string
+int main()
+{
+    //do polskich znaków
+    setlocale(LC_ALL, "polish");
+
+    int n = 3; //ilość nazwisk
+
+    string* lastNameArray = new string[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Nazwisko : ";
+        cin >> lastNameArray[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Nazwisko " << i << " : ";
+        cout << lastNameArray[i] << endl;
+    }
+
+    delete[] lastNameArray;
+
+    cout << "Program zakończył swoją pracę" << endl;
+}
