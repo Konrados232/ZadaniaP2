3520f797887bbfe5b6d803a4b50fc6e4829d3d61
Konrad Nowak <shaderpl@gmail.com>
2021-04-17 13:11:34 +0200

    Pierwsza wersja

---
+++ Zadanie 4/Zadanie 4.cpp
@@ -0,0 +1,37 @@
+﻿// Konrad Nowak
+// Zadanie 4
+/* Proszę napisać program z wyrażeniem regularnym, które będzie szukać wszystkich wyrazów nie zaczynających się na wielką lub małą literę E i zawierających małą literę a. Program ma wypisywać wyniki. */
+
+#include <iostream>
+#include <string>
+#include <regex>
+#include <algorithm>
+
+using namespace std;
+
+
+
+int main()
+{
+    string text = "  ewa    Ewa    eaaaw edfdfd   ewwwww   eeaaawwww   ffffwwwwwwraaaafafafaEwafaf   ";
+
+    regex mainRegex("[^a-zA-Z][E|e]{1}[a-zA-Z]*[a]{1}[a-zA-Z]*");
+
+    smatch match;
+
+    while (regex_search(text, match, mainRegex))
+    {
+
+        cout << match[0] << endl;
+        /*
+        for (int i = 0; i < match.size(); i++)
+        {
+            cout << match[i];
+        }
+        */
+        cout << endl;
+
+        text = match.suffix();
+    }
+
+}
